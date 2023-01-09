7ca6dd1a122db70d4a1cfa92c6ea044a72998b86
咯咯哒 <1075702919@qq.com>
2023-01-09 11:17:06 +0800

    1.push2

--- src/main/java/cn/cmm/Hello.java
+++ src/main/java/cn/cmm/Hello.java
@@ -16,5 +16,6 @@ public class Hello {
         System.out.println("master test");
         System.out.println("hot-fix");
         System.out.println("push");
+        System.out.println("push2");
     }
 }
