4fb645389e241cec1d0ac3bbb062e627a0dd51d2
咯咯哒 <1075702919@qq.com>
2023-01-09 10:53:51 +0800

    1.第一次提交

---
+++ src/main/java/cn/cmm/Hello.java
@@ -0,0 +1,17 @@
+package cn.cmm;
+
+/**
+ * @BelongsProject: git-dd
+ * @BelongsPackage: cn.cmm
+ * @Author: yangyingpeng
+ * @CreateTime: 2023-01-09  10:51
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HELLO version1");
+        System.out.println("HELLO version2");
+        System.out.println("HELLO version3");
+    }
+}
